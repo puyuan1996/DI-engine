862072ef63d3dc4325f58839dec819a9a321693a
puyuan1996 <2402552459@qq.com>
2021-10-19 12:33:09 +0800

    polish(pu): polish masac config

--- dizoo/smac/config/smac_MMM_masac_config.py
+++ dizoo/smac/config/smac_MMM_masac_config.py
@@ -2,12 +2,12 @@ from easydict import EasyDict
 from ding.entry import serial_pipeline
 
 agent_num = 10
-collector_env_num = 8
-evaluator_env_num = 8
+collector_env_num = 1  # TODO(pu) 8
+evaluator_env_num = 1  # TODO(pu) 8
 special_global_state = True
 
-cartpole_sac_default_config = dict(
-    exp_name='smac_MMM_ppo',
+MMM_masac_default_config = dict(
+    exp_name='debug_smac_MMM_masac',
     env=dict(
         map_name='MMM',
         difficulty=7,
@@ -16,9 +16,9 @@ cartpole_sac_default_config = dict(
         agent_num=agent_num,
         collector_env_num=collector_env_num,
         evaluator_env_num=evaluator_env_num,
-        n_evaluator_episode=16,
+        n_evaluator_episode=8,  # TODO(pu) 16,
         stop_value=0.99,
-        death_mask=False,
+        death_mask=True,  # TODO(pu) False
         special_global_state=special_global_state,
         # save_replay_episodes = 1,
         manager=dict(
@@ -29,30 +29,31 @@ cartpole_sac_default_config = dict(
     policy=dict(
         cuda=True,
         on_policy=False,
-        random_collect_size=0,
+        random_collect_size=0,  # TODO 10000
         model=dict(
             agent_obs_shape=186,
             global_obs_shape=389,
             action_shape=16,
             twin_critic=True,
-            actor_head_hidden_size=128,
-            critic_head_hidden_size=128,
+            actor_head_hidden_size=256,
+            critic_head_hidden_size=256,
         ),
         learn=dict(
-            update_per_collect=5,
-            batch_size=128,
-            learning_rate_q=1e-3,
-            learning_rate_policy=1e-3,
-            learning_rate_alpha=3e-4,
+            update_per_collect=50,  # TODO(pu) 5,
+            batch_size=320,  # TODO(pu) 64,
+            learning_rate_q=5e-4,
+            learning_rate_policy=5e-4,
+            learning_rate_alpha=5e-4,
             ignore_done=False,
-            target_theta=0.1,
+            target_theta=0.005,  # TODO(pu)
             discount_factor=0.99,
-            alpha=0.2,
-            auto_alpha=False,
+            alpha=0.2,  # TODO(pu) 0.5,
+            auto_alpha=True,  # TODO(pu)True,
+            log_space=True,
         ),
         collect=dict(
-            env_num=8,
-            n_sample=256,
+            env_num=collector_env_num,
+            n_sample=3200,  # TODO（pu）256,
             unroll_len=1,
         ),
         command=dict(),
@@ -60,16 +61,23 @@ cartpole_sac_default_config = dict(
             evaluator=dict(
                 eval_freq=50,
             ),
-            env_num=8,
+            env_num=evaluator_env_num,
         ),
-        other=dict(replay_buffer=dict(replay_buffer_size=1000000,max_use=20, ), ),
+        other=dict(
+            eps=dict(
+                type='linear',
+                start=1,
+                end=0.05,
+                decay=100000,
+            ),  # TODO(pu)
+            replay_buffer=dict(replay_buffer_size=1000000, ), ),
     ),
 )
 
-cartpole_sac_default_config = EasyDict(cartpole_sac_default_config)
-main_config = cartpole_sac_default_config
+MMM_masac_default_config = EasyDict(MMM_masac_default_config)
+main_config = MMM_masac_default_config
 
-cartpole_sac_default_create_config = dict(
+MMM_masac_default_create_config = dict(
     env=dict(
         type='smac',
         import_names=['dizoo.smac.envs.smac_env'],
@@ -80,8 +88,8 @@ cartpole_sac_default_create_config = dict(
     ),
     #replay_buffer=dict(type='naive', ),
 )
-cartpole_sac_default_create_config = EasyDict(cartpole_sac_default_create_config)
-create_config = cartpole_sac_default_create_config
+MMM_masac_default_create_config = EasyDict(MMM_masac_default_create_config)
+create_config = MMM_masac_default_create_config
 
 
 if __name__ == "__main__":
