c0d7cb9c6853bb0c38fdf4ac146e2a1da4c2a1b3
puyuan1996 <2402552459@qq.com>
2021-11-20 18:16:19 +0800

    polish(pu):polish config

--- dizoo/smac/config/smac_MMM2_qmix_config.py
+++ dizoo/smac/config/smac_MMM2_qmix_config.py
@@ -89,7 +89,7 @@ def train(args):
 
 if __name__ == "__main__":
     import argparse
-    for seed in [0,1,2]:     
+    for seed in [2]:     
         parser = argparse.ArgumentParser()
         parser.add_argument('--seed', '-s', type=int, default=seed)
         args = parser.parse_args()
